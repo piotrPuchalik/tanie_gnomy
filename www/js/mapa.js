7933ba9e64e40ebe48561b9ce6c6d2f2cd16fb5f
piotrPuchalik <piotrpuchalik@outlook.com>
2023-01-20 20:26:36 +0100

    login.css error

--- www/js/mapa.js
+++ www/js/mapa.js
@@ -1,6 +1,6 @@
 function inicjalizacjaMapy(x,y,div)
 {
-	map = L.map(div).setView([x, y], 17); //inicjalizacja mapy (współrzędne)
+	map = L.map(div).setView([x, y], 18); //inicjalizacja mapy (współrzędne)
 	
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
@@ -12,10 +12,10 @@ function inicjalizacjaMapy(x,y,div)
 
 WI1=document.getElementById('mapWI1');
 WI2=document.getElementById('mapWI2');
-WI1_X=53.447074		//współrzędna geograficzna X budynku WI1
-WI1_Y=14.492164		//współrzędna geograficzna Y budynku WI1
-WI2_X=53.448602		//współrzędna geograficzna X budynku WI2
-WI2_Y=14.491277		//współrzędna geograficzna Y budynku WI2
+WI1_X=53.447024		//współrzędna geograficzna X budynku WI1
+WI1_Y=14.492384		//współrzędna geograficzna Y budynku WI1
+WI2_X=53.448702		//współrzędna geograficzna X budynku WI2
+WI2_Y=14.491027		//współrzędna geograficzna Y budynku WI2
 
 
 inicjalizacjaMapy(WI1_X,WI1_Y,WI1) // utworzenie mapy dla budynku WI1
