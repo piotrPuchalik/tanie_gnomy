c40502a0269c08597f050b9b90dbdb16d44e931f
piotrPuchalik <piotrpuchalik@outlook.com>
2023-01-21 18:20:27 +0100

    dodane ikonki wydzaiału informatyki do stron

--- www/js/mapa.js
+++ www/js/mapa.js
@@ -1,4 +1,4 @@
-function inicjalizacjaMapy(x,y,div)
+function inicjalizacjaMapy(x,y,x1,y2,x2,y2,div)
 {
 	map = L.map(div).setView([x, y], 18); //inicjalizacja mapy (współrzędne)
 	
@@ -7,7 +7,8 @@ function inicjalizacjaMapy(x,y,div)
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	}).addTo(map); //Utworzenie mapy
 	
-	var marker = L.marker([x, y]).addTo(map); //Dodanie znacznika do mapy
+	var marker = L.marker([x1, y1]).addTo(map); //Dodanie znacznika do mapy
+	var marker = L.marker([x2, y2]).addTo(map);
 }
 
 WI1=document.getElementById('mapWI1');
@@ -17,9 +18,10 @@ WI1_Y=14.492384		//współrzędna geograficzna Y budynku WI1
 WI2_X=53.448702		//współrzędna geograficzna X budynku WI2
 WI2_Y=14.491027		//współrzędna geograficzna Y budynku WI2
 
+srodek_x = Math.abs(WI2_X+((WI1_X-WI2_X)/2));
+srodek_y = Math.abs(WI2_Y+((WI1_Y-WI2_Y)/2));
 
-inicjalizacjaMapy(WI1_X,WI1_Y,WI1) // utworzenie mapy dla budynku WI1
-inicjalizacjaMapy(WI2_X,WI2_Y,WI2) // utworzenie mapy dla budynku WI2
+inicjalizacjaMapy(srodek_x,srodek_y,WI1_X,WI1_Y,WI2_X, WI2_Y, WI1) // utworzenie mapy dla budynku WI1
 
 function planWI1(){
 	window.open("plan_wi1.html", "_self");
